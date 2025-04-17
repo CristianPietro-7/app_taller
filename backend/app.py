f42b3629f855e93f8da1aa7fcbd5faa7f95c3dbd
CristianPietro-7 <cristian.r.pietrobon@gmail.com>
2025-04-17 20:15:32 -0300

    database connection

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from config.config import DATABASE_CONNECTION_URI
+from flask_cors import CORS
+from models.db import db
+
 
 app = Flask(__name__)
+
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"]= DATABASE_CONNECTION_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 @app.route('/')
 def home():
