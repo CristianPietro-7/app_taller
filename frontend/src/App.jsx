4503697e81f4b2cdf445b9e6e0fe5922b6112227
CristianPietro-7 <cristian.r.pietrobon@gmail.com>
2025-04-19 22:45:52 -0300

    the front of the vehicle was modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import VehiculosPage from "./components/Vehiculos/vehiculoPage";
+import VehiculosPage from "./components/Vehiculos/VehiculoPage";
 
 export default function App() {
   return (
