504ebb2c24404e39029f20dcf39b178b32fb11e7
CristianPietro-7 <cristian.r.pietrobon@gmail.com>
2025-04-19 22:07:44 -0300

    front vehículos

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import VehiculosPage from "./components/Vehiculos/vehiculoPage";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/vehiculos" element={<VehiculosPage />} />
+      </Routes>
+    </Router>
+  );
+}
